63f5247aa9490b3849ecadf2adbe7c0e6feb9c0f
ErenPaper <raphaelramos19@gmail.com>
2023-02-03 13:29:18 -0700

    February 3

--- Weekly Assignment Quizzes/WE Quiz #2/Question1.cpp
+++ Weekly Assignment Quizzes/WE Quiz #2/Question1.cpp
@@ -5,19 +5,19 @@ int main() {
 // int * const ptr = NULL, var;     #Error a)
 // ptr = &var;
 
-// long *ptr = 0x7ffeebfa2a9c;      #Error b)
+// long *ptr = 0x7ffeebfa2a9c;  
 
 
 // char word[20] = "first name";    #Error c)
 // &word[4] = NULL;
 
 
-unsigned int var = 10;              d)
-const unsigned int * ptr = &var;
-var += *ptr;
+// unsigned int var = 10;              d)
+// const unsigned int * ptr = &var;
+// var += *ptr;
 
-short * ptr = NULL, var;            e)
-ptr = &var;
+// short * ptr = NULL, var;            e)
+// ptr = &var;
 
 
 return 0;
--- Weekly Assignment Quizzes/WE Quiz #2/Question4.cpp
+++ Weekly Assignment Quizzes/WE Quiz #2/Question4.cpp
@@ -3,4 +3,10 @@
 int main() {
     int * ptr;
     int ** d_ptr = &ptr;
+
+    std::cout << ptr + 1 << "\n";
+    std::cout << d_ptr + 1 << "\n";
+    std::cout << &d_ptr[1] << "\n";
+    std::cout << &ptr + 1 << "\n";
+    
 }
